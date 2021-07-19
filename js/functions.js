db381b946cdd3a8f2b890565403dd01145297461
davidebortoloni <davidebortoloni@gmail.com>
2021-07-19 15:24:42 +0200

    Inserimento secondo script (mancano i commenti)

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,45 @@ function print(id, result) {
         element.innerHTML = result;
     }
 }
+function displayStudent(id, students) {
+    let studentsInfo = "";
+    for (let i = 0; i < students.length; i++) {
+        studentsInfo += "<li>Name: " + students[i].name + "</li>";
+        studentsInfo += "<li>Surname: " + students[i].surname + "</li><br>";
+    }
+    print(id, studentsInfo);
+}
+function isValidName(inputTxt) {
+    var letters = /^[a-zA-Z\s]+$/;
+    if (inputTxt.match(letters)) {
+        return true;
+    }
+    else {
+        alert("Please enter a valid name (without numbers and special characters)");
+        return false;
+    }
+}
+function isValidSurname(inputTxt) {
+    var letters = /^[a-zA-Z\s]+$/;
+    if (inputTxt.match(letters)) {
+        return true;
+    }
+    else {
+        alert("Please enter a valid surname (without numbers and special characters)");
+        return false;
+    }
+}
+function isValidAge(age) {
+    if (!age || age < 18 || age == "" || isNaN(age)) {
+        alert("Please enter a valid age");
+        return false;
+    }
+    return true;
+}
+function capitalize(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(' ');
+}
--- js/script.js
+++ js/script.js
@@ -1,18 +1,73 @@
-// ES 1
+// ES 2
 
-// Creare un oggetto che descriva uno studente,
-// con le seguenti proprietà: nome, cognome e età.
+// Dare la possibilità all’utente, attraverso
+// 3 prompt(), di aggiungere un nuovo oggetto studente
+// inserendo nell’ordine: nome, cognome e età.
 
-// Stampare a schermo (in pagina) attraverso un
-// ciclofor-in tutte le proprietà dell'oggetto.
-
-const student = {
+const addButton = document.getElementById("add-student");
+const bestStudent = {
     name: "Davide",
     surname: "Bortoloni",
     age: 22,
 };
-let result = "";
-for (let key in student) {
-    result += key + ": " + student[key] + "<br>";
+const students = [
+    {
+        name: "Marilena",
+        surname: "Cantisani",
+        age: 24,
+    },
+    {
+        name: "Giorgio",
+        surname: "Andrei",
+        age: 21,
+    },
+    {
+        name: "Giuseppe",
+        surname: "Pisani",
+        age: 28,
+    },
+    {
+        name: "Pasquale",
+        surname: "Raso",
+        age: 48,
+    },
+    {
+        name: "Elena",
+        surname: "Durante",
+        age: 25,
+    },
+];
+
+let bestStudentInfo = "";
+for (let key in bestStudent) {
+    bestStudentInfo += key + ": " + bestStudent[key] + "<br>";
 }
-print("result", result);
+print("best-student", bestStudentInfo);
+
+displayStudent("students", students)
+
+addButton.onclick = function () {
+    let newStudentAge;
+    let newStudentName;
+    let newStudentSurname;
+
+    do {
+        newStudentName = prompt("Enter the name").trim();
+    } while (!isValidName(newStudentName));
+    do {
+        newStudentSurname = prompt("Enter the surname").trim();
+    } while (!isValidSurname(newStudentSurname));
+    do {
+        newStudentAge = prompt("Enter the age").trim();
+    } while (!isValidAge(newStudentAge));
+
+    const newStudent = {
+        name: capitalize(newStudentName),
+        surname: capitalize(newStudentSurname),
+        age: newStudentAge,
+    };
+
+    students.push(newStudent);
+
+    displayStudent("students", students)
+};
